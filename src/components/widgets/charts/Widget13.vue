fdfb78303306c21e9878d3f2f90a70b47c41d947
alsajet12 <a.molla@kuramasrl.com>
2025-11-11 17:55:02 +0100

    mappa

--- src/components/widgets/charts/Widget13.vue
+++ src/components/widgets/charts/Widget13.vue
@@ -282,11 +282,11 @@ export default defineComponent({
       map = L.map(mapContainer.value, {
         center: mainCoords,
         zoom: 9.56, // Initial zoom, will be adjusted based on visible agencies
-        zoomControl: false,
-        scrollWheelZoom: true,
+        zoomControl: true,
+        scrollWheelZoom: false,
         doubleClickZoom: false,
         dragging: true,
-        touchZoom: true,
+        touchZoom: false,
         boxZoom: true,
         keyboard: false
       });
